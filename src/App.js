788a4b958fbbed07c796524ba5f8710d54d7b254
apache-0002 <vinyy64@live.com>
2021-08-27 20:56:50 -0300

    version1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles.css';
+import { ToastContainer, toast } from 'react-toastify'; 
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+export default function App() {
+
+    const diffToast  = () => {
+      toast("Login sucessfull!", {
+        position: "top-center",
+      });
+    }
+
   return (
-    <div className="App">
-      
+    <div className="main-div">
+          <h2>Welcome to signin page</h2>
+          <button className="btn btn-primary" onClick={diffToast}>Login</button>
+          <ToastContainer />
     </div>
   );
 }
 
-export default App;
